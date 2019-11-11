f13d1895c05838660f0760599e38b5a8cf7af720
Andreas Tvete <andreas.roed.tvete@gmail.com>
2019-11-11 14:41:07 +0100

    Optimized local storage

--- storage.js
+++ storage.js
@@ -3,32 +3,22 @@ const os = require('os');
 
 storage.setDataPath(os.tmpdir());
 
-function rememberFingerprint() {
+function rememberInfo() {
   fingerPrint = document.getElementById("fingerprint").value;
-  storage.set('fingerprintdata', { fingerprint: fingerPrint }, function(error) {
-    if (error) throw error;
-  });
-}
-
-function rememberColor() {
   colorFromHTML = document.getElementById("color").value;
-  storage.set('colordata', { color: colorFromHTML }, function(error) {
+  storage.set('application', { color: colorFromHTML, fingerprint: fingerPrint }, function(error) {
     if (error) throw error;
   });
 }
 
 function rememberedValues() {
-  storage.get('fingerprintdata', function(error, data) {
+  storage.get('application', function(error, data) {
     if (error) throw error;
     if (data.fingerprint === undefined)
       data.fingerprint = '';
-    document.getElementById("fingerprint").value = data.fingerprint;
-  });
-
-  storage.get('colordata', function(error, data) {
-    if (error) throw error;
     if (data.color === undefined)
       data.color = '';
+    document.getElementById("fingerprint").value = data.fingerprint;
     document.getElementById("color").value = data.color;
   });
 }
